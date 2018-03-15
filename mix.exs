101f99a68e4a71ece9320d41f6dca75359d370a9
Tim Littlemore <tim@tjl2.com>
2018-03-15 21:02:55 +0000

    Added ex_doc & earmak

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Issues.MixProject do
       escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.6",
+      name: "Issues",
+      source_url: "https://github.com/tjl2/issues",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -25,7 +27,9 @@ defmodule Issues.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:httpoison, "~> 1.0"},
-      {:poison, "~> 3.1.0"}
+      {:poison, "~> 3.1.0"},
+      {:ex_doc, "~> 0.12"},
+      {:earmark, "~> 1.0", override: true}
     ]
   end
 
