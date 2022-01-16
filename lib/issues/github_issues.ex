44c4732dfab0c925c8d198cfd1992f465d06abe2
Tim Littlemore <tim@tjl2.com>
2022-01-16 21:11:54 +0000

    Make github_url configurable

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,5 +1,6 @@
 defmodule Issues.GitHubIssues do
   @user_agent [{"User-agent", "Elixir tim@tjl2.com"}]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, repo) do
     issues_url(user, repo)
@@ -8,13 +9,13 @@ defmodule Issues.GitHubIssues do
   end
 
   def issues_url(user, repo) do
-    "https://api.github.com/repos/#{user}/#{repo}/issues"
+    "#{@github_url}/repos/#{user}/#{repo}/issues"
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
     {
       status_code |> check_for_error(),
-      body |> Poison.Parser.parse!()
+      body |> Poison.Parser.parse!() # A list of maps, each map is a GH issue
     }
   end
 
