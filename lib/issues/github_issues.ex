708913e7bff006b80ca205b5f7891e7ce7a84a1b
Tim Littlemore <tim@tjl2.com>
2022-01-18 17:12:24 +0000

    Add Logging
    Set to purge log levels lower than info on compile. This means now debug logs when using `mix run` or an escript build

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,8 +1,11 @@
 defmodule Issues.GitHubIssues do
+  require Logger
   @user_agent [{"User-agent", "Elixir tim@tjl2.com"}]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, repo) do
+    Logger.info("Fetching #{user}'s repo #{repo}")
+
     issues_url(user, repo)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -13,6 +16,9 @@ defmodule Issues.GitHubIssues do
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code = #{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       status_code |> check_for_error(),
       # A list of maps, each map is a GH issue
